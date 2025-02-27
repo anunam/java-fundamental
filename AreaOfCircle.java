73da2a74d7886ce227a529cdcd5f0ce5eddd25d6
Anupam Jana <159534615+anunam@users.noreply.github.com>
2025-02-27 13:56:47 +0530

    Area of circle

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -3,7 +3,7 @@ public class AreaOfCircle{
 public static void main(String []args){
 Scanner input=new Scanner(System.in);
 System.out.println("Enter the radius :");
-double r=input.nextInt();
+double r=input.nextDouble();
 System.out.print("Area of the circle is :"+Math.PI*r*r);
 }
 }
