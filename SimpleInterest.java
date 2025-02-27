852666bebaf5789c7406cfe8df9052649a2ce7f8
Anupam Jana <159534615+anunam@users.noreply.github.com>
2025-02-27 14:05:11 +0530

    simple interest

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -3,11 +3,11 @@ public class SimpleInterest{
 public static void main(String []args){
 Scanner input=new Scanner(System.in);
 System.out.println("Enter the principle :");
-double p=input.nextInt();
+double p=input.nextDouble();
 System.out.println("Enter the rate :");
-double r=input.nextInt();
+double r=input.nextDouble();
 System.out.println("Enter the time :");
-double t=input.nextInt();
+double t=input.nextDouble();
 System.out.print("Simple interest is :"+(p*r*t)/100);
 }
 }
