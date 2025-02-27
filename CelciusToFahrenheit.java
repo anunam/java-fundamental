da8096275757b1cf4bc8f642ea482792682203a3
Anupam Jana <159534615+anunam@users.noreply.github.com>
2025-02-27 13:59:37 +0530

    celcius to fahrenheit

--- CelciusToFahrenheit.java
+++ CelciusToFahrenheit.java
@@ -3,7 +3,7 @@ public class CelciusToFahrenheit{
 public static void main(String []args){
 Scanner input=new Scanner(System.in);
 System.out.println("Enter the Celcius to convert Fahrenheit :");
-double x=input.nextInt();
+double x=input.nextDouble();
 double value=(x*(9/5))+32;
 System.out.print("fahrenheit value is :"+value);
 
