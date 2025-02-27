59f7d6b658f123d73e765412c6ae36802e84dc72
anunam <anupamjana478@gmail.com>
2025-02-27 13:23:02 +0530

    Perimeter of rectangle

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PerimeterOfRectangle{
+public static void main(String []args){
+Scanner input=new Scanner(System.in);
+System.out.println("Enter the length :");
+double l=input.nextInt();
+System.out.println("Enter the width :");
+double w=input.nextInt();
+System.out.print("Area of the cylinder is :"+2*(l+w));
+}
+}
