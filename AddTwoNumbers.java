86731f5bebf3d222640e927ee6ece03cb99ddb78
anunam <anupamjana478@gmail.com>
2025-02-27 11:40:37 +0530

    Add to number

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AddTwoNumbers{
+public static void main(String []args){
+Scanner input=new Scanner(System.in);
+System.out.println("Enter first number :");
+int x=input.nextInt();
+System.out.println("Enter second number :");
+int y=input.nextInt();
+System.out.println("Sum of two numbers are :");
+System.out.print(x+y);
+}
+}
